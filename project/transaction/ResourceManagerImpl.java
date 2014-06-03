0de2de9b5a40610c215fd40e1eeb349f680ad183
kewlgandhi <kewlgandhi@gmail.com>
2014-06-02 22:00:06 -0700

    unused import

--- project/transaction/ResourceManagerImpl.java
+++ project/transaction/ResourceManagerImpl.java
@@ -21,8 +21,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.Callable;
-
 import org.apache.log4j.Logger;
 
 /** 
